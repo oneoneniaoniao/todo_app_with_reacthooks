2d78d1dd6050c8708d63f36f1a7fa6b2c957dd7f
oneoneniaoniao <321marumaru123@gmail.com>
2021-07-26 13:24:26 +0900

    add h3 in complete list

--- src/Components/CompleteList.js
+++ src/Components/CompleteList.js
@@ -4,7 +4,7 @@ import CompleteToDo from "./CompleteToDo";
 import AppContext from "../context/AppContext"
 
 
-const CompleteList = ({title}) => {
+const CompleteList = () => {
     const deleteAllCompleteTodos = (e) => {
       e.preventDefault();
       const result = window.confirm("全ての完了したTODOを本当に削除しても良いですか？");
@@ -17,7 +17,7 @@ const CompleteList = ({title}) => {
 
     return (
       <div className="m-12">
-        <h3 className="text-2xl inline-block">{title}</h3>
+        <h3 className="text-2xl inline-block">完了したToDo</h3>
                  <button className="text-sm ml-8 bg-red-300 text-white rounded-lg p-1 px-2 m-1 hover:bg-red-500" onClick={deleteAllCompleteTodos} disabled={state.completeTodos.length === 0}>全ての完了したToDoを削除する</button>
         <table className="m-4">
         <thead>
